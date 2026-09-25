d21d81aaac50ece2912b684ba520ae78a6f4a77f
= <=>
2026-09-26 00:57:18 +0530

    initial BPMS project has setup

---
+++ bpms.project/src/main/java/com/suraj/bpms/project/Application.java
@@ -0,0 +1,18 @@
+package com.suraj.bpms.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		SpringApplication.run(Application.class, args);
+
+
+		System.out.println("Application Started");
+	}
+
+}
